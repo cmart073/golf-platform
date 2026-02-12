c8c33a6f79b8b2a622c86abe836ae1dd6ddc984d
Chris Martin <cmart073@gmail.com>
2026-02-12 16:42:41 -0600

    Initial golf platform

--- migrations/0002_seed.sql
+++
@@ -1,5 +0,0 @@
--- Migration 0002: Seed demo organization
--- Run with: wrangler d1 execute golf-db --remote --file=migrations/0002_seed.sql
-
-INSERT OR IGNORE INTO organizations (id, slug, name, created_at)
-VALUES ('org_demo_00000001', 'demo-course', 'Demo Course', datetime('now'));
